2af8307f11f6659753a1a8d3e3116ff5cce3d6bc
Elita Hajrizi <eh47718@ubt-uni.net>
2021-02-14 19:31:03 +0100

    tweaks

--- phpFiles/dashboard.php
+++ phpFiles/dashboard.php
@@ -1,14 +1,44 @@
 <?php
-include 'header.php';
-include_once '../businessLogic/variables.php';
+
 include_once 'userMapper.php';
-if (isset($_SESSION['role']) && $_SESSION['role'] == '1') {
+if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
     $mapper =  new UserMapper();
     $userList = $mapper->getAllUsers();
 } else {
     header("Location:./phpPages/getInvolved.php");
 }
 ?>
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Find your roots</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+	
+    <body>
+        <div class ="all-backgound">
+            <div class="header">
+                <a href="../phpPages/getInvolved.php">Get involved</a>
+
+                <div class="dropdown">
+                <button class="dropbtn">Discover
+                    <i class="fa fa-caret-down"></i>
+                </button>
+					<div class="dropdown-content">
+						<a href="../phpPages/books.php">Books</a>
+						<a href="Music.php">Music</a>
+						<a href="../phpPages/Places.php">Places</a>
+					</div>
+                </div>
+
+                <a href="../phpPages/aboutUs.php">About Us</a>
+                <a href="../phpPages/home.php">Home</a>
+                <a href="../phpFiles/dashboard.php">Dashboard</a>
+                
+            </div>
+
 
 <div>
     <h1>This is the Dashboard page</h1>
