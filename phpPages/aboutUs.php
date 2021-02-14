6f5fb587675e4178107c0aa6bf2607e75ebaf4b6
Albana Hajdini <ah48391@ubt-uni.net>
2021-02-14 21:03:35 +0100

    responsivity edhe css i faqeve update

--- phpPages/aboutUs.php
+++ phpPages/aboutUs.php
@@ -33,7 +33,7 @@
                     <div class="aboutUs-inner-container">
                         <h1>About Us</h1>
                         <p class="text">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                            Lorem Ipsum is simply dummy text 
                         </p>
                     </div>
                 </div>
