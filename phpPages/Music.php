bfd306cc6c4707f4149fbd94254ecbe20afc4583
Albana Hajdini <ah48391@ubt-uni.net>
2021-02-14 13:29:45 +0100

    music page update, artist update

--- phpPages/Music.php
+++ phpPages/Music.php
@@ -1,42 +1,75 @@
 <?php
 include '../phpFiles/header.php';
 ?>
-<div class="music-main-content">
-
-    <div class="music-left-container">
-        <div class="left-subcontainer1">
-            <h2>TITLE HEADING</h2>
-            <h5>Title </h5>
-            <div class="contentimg" style="height:100px;">Content</div>
+<div class="music_main_content">
+    <h1> Artists </h1>
         
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+   <div class= "music_artists" >
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
+
         </div>
 
-        <div class="left-subcontainer2">
-            <h2>TITLE HEADING</h2>
-            <h5>Title </h5>
-            <div class="contentimg" style="height:100px;">Content</div>
-            <p>Sunt....</p>
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
+
         </div>
 
-    </div>
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
 
-  <div class="music-right-container">
-        <div class="right-subcontainer1">
-            <h2>Title</h2>
-            <div class="contentimg" style="height:100px;">Content</div>
-            
-        </div>
-
-        <div class="right-subcontainer2">
-            <h3>Title</h3>
-            <div class="contentimg"><p>Content</p></div>
-            <div class="contentimg"><p>Content</p></div>
-            <div class="contentimg"><p>Content</p></div>
         </div>
 
         
-  </div>
+
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
+
+        </div>
+        
+
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
+
+        </div>    
+        
+        <div class ="artist_info"> 
+            <div class ="artist_img">
+                <img src ="" alt="artist_info_img">
+            </div>
+            <h3> John Doe </h3>
+            <p class="role">pop artust</p>
+            <p>Lorem ispun</p>
+
+        </div>    
+   </div>
+   
+
 </div>
  
    
