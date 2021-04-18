ac503f9b9e8fa9e03434f45dbba1fa25325826fe
Albana Hajdini <ah48391@ubt-uni.net>
2021-04-18 20:02:04 +0200

    ku eshte problemi

--- phpPages/Places.php
+++ phpPages/Places.php
@@ -7,58 +7,99 @@ $result = new UserMapper();
 $result = $result -> getAllPlaces();
 ?>
 			
-			<div class="places-main-content">
-				<h1>Tourist Atractions</h1>
+	<div class="places-main-content">
+		<h1>Tourist Atractions</h1>
 				
-						<div class="places-left">
-							<div class="map">
-								<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d777233.1596769514!2d20.433676996994624!3d42.59891903316791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee605110927%3A0x9365bfdf385eb95a!2sPristina!5e0!3m2!1sen!2sus!4v1613311328681!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
-							</div>
+			<div class="places-left">
+				<div class="map">
+					<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d777233.1596769514!2d20.433676996994624!3d42.59891903316791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549ee605110927%3A0x9365bfdf385eb95a!2sPristina!5e0!3m2!1sen!2sus!4v1613311328681!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
+				</div>
 							
-						</div>				
-					   <div class="places-right">
-						   <div class="places-slider">
+			</div>				
+			<div class="places-right">
+				<div class="places-slider">
 
 
-								<?php 
-								$result = array_chunk($result,1);
+					<?php 
+						$result = array_chunk($result,1);
 
-									foreach ($result as $result) {
-									echo " <div class='places-slider-item'>";
-										foreach ($result as $item) {  ?>   
+						foreach ($result as $result) {
+						echo " <div class='places-slider-item'>";
+						foreach ($result as $item) {  ?>   
 
 
 									  
-											<img src ="<?php echo $item['link'];?>" alt="<?php echo $item['name'];?>">	
+						<img src ="<?php echo $item['link'];?>" alt="<?php echo $item['name'];?>">	
 
-											<div class="places-slider-item-text">
-												<h1><?php echo $item['name'];?></h1>
+						<div class="places-slider-item-text">
+							<h1><?php echo $item['name'];?></h1>
 												
-												<p><?php echo $item['description'];?></p> 
+							<p><?php echo $item['description'];?></p> 
 
-											</div>
+						</div>
 
-								<?php
-											}  
-											echo "</div>";
+					<?php
+						}  
+						echo "</div>";
 										
-										}
-								?>
+						}
+					?>
 
 
-							</div>
+			    </div>
 		
-					   </div>
-						
+		    </div>
 
-					   <?php if(isset($_SESSION["role"]) && $_SESSION["role"]==1):?>
-			   <br>
-			   <button id="places_button"><a class="linkbtn" href="../phpFiles/addPlaces.php?>">Add Place</a></button>
-			   <?php endif;?>
+
+			
+
+
+
+					
+
+							
+					<?php 
+						$result = array_chunk($result,1);
+
+						foreach ($result as $result) {
+						echo "<div class='main_activity'>";
+						echo "<div class='row_activity'>";
+						echo "<div class='box_activity'>";
+						foreach ($result as $item) {  ?>   
+
+
+									  
+						<img src ="<?php echo $item['act_img'];?>" alt="<?php echo $item['name_act'];?>">	
+
+						<div class="activity_content">
+							<h3><?php echo $item['name_act'];?></h3>
+												
+							<p><?php echo $item['description_act'];?></p> 
+
+						</div>
+
+
+					<?php
+						}  
+						echo "</div>";
+						echo "</div>";
+						echo "</div>";
+										
+						}
+					?>
+
+			
+
+			
+	
+		<?php if(isset($_SESSION["role"]) && $_SESSION["role"]==1):?>
+		<br>
+		<button id="places_button"><a class="linkbtn" href="../phpFiles/addPlaces.php?>">Add Place</a></button>
+		<?php endif;?>
 
 					   
 					
-			</div>
+	</div>
 			
 <?php
 include '../phpFiles/footer.php';
